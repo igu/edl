515d1c2c16a6790350fd75007f4ad3c81a7c9a24
Igor Alisson <igoralissoncn@gmail.com>
2019-09-10 21:15:12 -0300

    Criando interface da estrutura de Lista

---
+++ Lista/Lista.java
@@ -0,0 +1,41 @@
+package Lista;
+
+public interface Lista {
+
+    /*
+      ==== METÓDOS GENERICOS ===== 
+    */
+    
+    public int size();
+    public boolean isEmpty();
+    public void display();
+    
+    /*
+      ==== METÓDOS DE FILA ===== 
+    */
+    
+    public No isFirst(No n);
+    public No isLast(No n);
+    
+    /*
+      ==== METÓDOS PARA ACESSAR ===== 
+    */
+    
+    public No first();
+    public No last();
+    public No before(No o);
+    public No after(No o);
+    
+    /*
+      ==== METÓDOS PARA ATUALIZAR ===== 
+    */
+    
+    public void replaceElement(No n, Object o);
+    public void swapElements(No trocado, No trocador);
+    public void insertBefore(No before, Object o);
+    public void insertAfter(No after, Object o);
+    public void insertFirst(Object o);
+    public void insertLast(Object o);
+    public void remove(No n);
+    
+}
