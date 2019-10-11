8dc4ccfc5ea4890b9789fd638c564c2c43f26786
Igor Alisson <igoralissoncn@gmail.com>
2019-10-11 12:58:19 -0300

    Adicionando interface da ArvoreBinaria

---
+++ ArvoreBinaria/ArvoreBinaria.java
@@ -0,0 +1,18 @@
+package ArvoreBinaria;
+
+public interface ArvoreBinaria {
+
+    public void inserir(Object elem);
+    public No buscar(Object elem) throws ElementoNaoEncontradoException;
+    public boolean remover(Object elem);
+    public ArrayList<No> nos(int mode);
+    public int height(No no);
+    public int profundidade(No no);
+    public boolean isRoot(No no);
+    public boolean containsChildEsquerdo(No no);
+    public boolean containsChildDireito(No no);
+    public int countChilds(No no);
+    public boolean isExternal(no no);
+    public void display();
+    
+}
