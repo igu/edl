af109d1c706d92bd41741f862efeca424bd65fa2
Igor Alisson <igoralissoncn@gmail.com>
2019-10-12 18:09:33 -0300

    Alterando metodos da interface e adicionando importacoes

--- ArvoreBinaria/ArvoreBinaria.java
+++ ArvoreBinaria/ArvoreBinaria.java
@@ -1,9 +1,11 @@
 package ArvoreBinaria;
 
+import java.util.ArrayList;
+
 public interface ArvoreBinaria {
 
     public void inserir(Object elem);
-    public No buscar(Object elem) throws ElementoNaoEncontradoException;
+    public No buscar(Object elem) throws ElNaoEncontradoException;
     public boolean remover(Object elem);
     public ArrayList<No> nos(int mode);
     public int height(No no);
@@ -12,7 +14,7 @@ public interface ArvoreBinaria {
     public boolean containsChildEsquerdo(No no);
     public boolean containsChildDireito(No no);
     public int countChilds(No no);
-    public boolean isExternal(no no);
+    public boolean isExternal(No no);
     public void display();
     
 }
