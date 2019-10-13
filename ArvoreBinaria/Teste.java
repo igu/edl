0f87b2eb9656d9722cefbecd333978244410f8d7
Igor Alisson <igoralissoncn@gmail.com>
2019-10-13 15:22:01 -0300

    Adicionando classe de teste para arvore binaria

---
+++ ArvoreBinaria/Teste.java
@@ -0,0 +1,12 @@
+package ArvoreBinaria;
+
+public class Teste {
+	public static void main(String[] args) {
+		ArvoreBinariaImpl novaTree = new ArvoreBinariaImpl(10);
+		
+		novaTree.inserir(11);
+		novaTree.inserir(8);
+		novaTree.inserir(10);
+		
+	}
+}
