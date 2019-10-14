d45ef602d999f8fcf817af2eb5744587dbc254c6
Igor Alisson <igoralissoncn@gmail.com>
2019-10-13 22:34:36 -0300

    Adicionando teste na classe

--- ArvoreBinaria/Teste.java
+++ ArvoreBinaria/Teste.java
@@ -2,13 +2,34 @@ package ArvoreBinaria;
 
 public class Teste {
 	public static void main(String[] args) {
-		ArvoreBinariaImpl novaTree = new ArvoreBinariaImpl(10);
+		ArvoreBinariaImpl novaTree = new ArvoreBinariaImpl(25);
 		
-		novaTree.inserir(11);
-		novaTree.inserir(8);
-		novaTree.inserir(9);
+		novaTree.inserir(20);
+		novaTree.inserir(18);
+		novaTree.inserir(25);
+		novaTree.inserir(15);
+		novaTree.inserir(23);
+		novaTree.inserir(28);
+		novaTree.inserir(31);
+		novaTree.inserir(22);
+		novaTree.inserir(21);
+		novaTree.inserir(24);
 		
-		novaTree.nos(1);
+		novaTree.display();
+		
+		
+		System.out.println("-----------------------------------" + novaTree.getSize());
+		
+		try {
+			novaTree.remover(20);
+		} catch (ElNaoEncontradoException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		novaTree.display();
+		
+		System.out.println("-----------------------------------" + novaTree.getSize());
 		
 	}
 }
