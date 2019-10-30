28bd42f2b533392193c927d1e57ab5948a2ab0b0
Igor Alisson <igoralissoncn@gmail.com>
2019-10-30 17:29:14 -0300

    Atualizando ordem dos metodos da interface

--- Heap/Heap.java
+++ Heap/Heap.java
@@ -4,8 +4,8 @@ public interface Heap {
 	public int size();
 	public boolean isEmpty();
 	public void insert(Object o);
-	public void removeMin();
 	public void upHeap();
+	public void removeMin();	
 	public void downHeap();
 	public void display();
 }
