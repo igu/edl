fdeaf829703adf803f9454704a6d60c65a3823fa
Igor Alisson <igoralissoncn@gmail.com>
2019-10-31 21:22:50 -0300

    Implementando página de teste

--- Heap/Teste.java
+++ Heap/Teste.java
@@ -2,9 +2,19 @@ package Heap;
 
 public class Teste {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
 
-	}
+        HeapImpl nova = new HeapImpl();
+
+        nova.insert(1);
+        nova.insert(5);
+        nova.insert(9);
+        nova.insert(7);
+        nova.insert(2);
+        nova.insert(6);
+
+        nova.display();
+        System.out.println("----------------------");
+    }
 
 }
