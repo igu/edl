e947b8978652341ccf90a973cf36137f285f7d38
Igor Alisson <igoralissoncn@gmail.com>
2019-10-31 21:21:29 -0300

    Adicionando metodo de display

--- Heap/HeapImpl.java
+++ Heap/HeapImpl.java
@@ -1,8 +1,12 @@
 package Heap;
 
+import java.lang.Math;
+import java.util.ArrayList;
+
 public class HeapImpl implements Heap {
 
     private Object heapArray[];
+    private ArrayList<Integer> inteirosHeap;
     private int size;
 
     public HeapImpl() {
@@ -53,6 +57,7 @@ public class HeapImpl implements Heap {
         do {
             childRightIndex = 2 * i + 1;
             childLeftIndex = 2 * i;
+
             if (childRightIndex <= this.size) {
                 if ((int) this.heapArray[childRightIndex] < (int) this.heapArray[childLeftIndex]) {
                     k = childRightIndex;
@@ -68,16 +73,76 @@ public class HeapImpl implements Heap {
                 this.heapArray[k] = (int) this.heapArray[i];
                 this.heapArray[i] = aux;
             }
-
             i = k;
         } while (childLeftIndex < this.size);
 
-        // Downheap termina quando a chave k encontra uma folha 
-        // ou um nó cuja chave é maior do que k
+        // Downheap termina quando a chave k encontra uma folha ou um nó 
+        // cuja chave é maior do que k
     }
 
     @Override
     public void display() {
+        int linhas = (int) ((Math.log(this.size) / Math.log(2))) + 1; // altura
+        int colunas = this.size, k = 0;
+        int matrix[][] = new int[linhas][colunas];
 
+        ArrayList<Integer> inteiros = this.inteiros();
+
+        while (k < this.size) {
+            matrix[(int) (Math.log(inteiros.get(k)) / Math.log(2))][k] = (int) this.heapArray[inteiros.get(k)];
+            k++;
+        }
+
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                if (matrix[i][j] != 0) {
+                    System.out.print(matrix[i][j] + "   ");
+                } else {
+                    System.out.print("   ");
+                }
+            }
+            System.out.print("\n");
+        }
+    }
+
+    @Override
+    public ArrayList<Integer> inteiros() {
+        this.inteirosHeap = new ArrayList<Integer>();
+        this.inOrdem(1);
+        return this.inteirosHeap;
+    }
+
+    private void inOrdem(int index) {
+        if (this.containsChildEsquerdo(index)) {
+            this.inOrdem(index * 2);
+        }
+
+        this.inteirosHeap.add(index);
+
+        if (this.containsChildDireito(index)) {
+            this.inOrdem(index * 2 + 1);
+        }
+    }
+
+    @Override
+    public boolean containsChildDireito(int index) {
+        index *= 2 + 1;
+        if (index <= this.size) {
+            if (this.heapArray[index] != null) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean containsChildEsquerdo(int index) {
+        index *= 2;
+        if (index <= this.size) {
+            if (this.heapArray[index] != null) {
+                return true;
+            }
+        }
+        return false;
     }
 }
