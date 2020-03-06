9b7c004e1ea6e65164d08051fb17142330651402
Igor Alisson <igoralissoncn@gmail.com>
2020-03-06 00:32:26 -0300

    criando metodo attFb e insert

--- ArvoreAVL/ArvoreAvllmpl.java
+++ ArvoreAVL/ArvoreAvllmpl.java
@@ -4,28 +4,69 @@ import ArvoreBinaria.ArvoreBinariaImpl;
 
 public class ArvoreAvllmpl extends ArvoreBinariaImpl implements ArvoreAvl {
 
-	@Override
-	public void attFb(String op) {
-		// TODO Auto-generated method stub
-		
-	}
+    public boolean inserir(Object elem) {
+        if (this.root != null) {
+            NoAVL pai = (NoAVL) this.buscar(elem, root);
+            if (pai.getElemento() == elem) {
+                return false; // ELEMENTO JÁ CONTIDO
+            }
+            NoAVL novoFilho = new NoAVL(elem, pai, null, null, 0);
 
-	@Override
-	public void rotSimLeft() {
-		// TODO Auto-generated method stub
-		
-	}
+            if ((int) pai.getElemento() > (int) novoFilho.getElemento()) {
+                pai.setChildEsquerdo(novoFilho);
+            } else {
+                pai.setChildDireito(novoFilho);
+            }
 
-	@Override
-	public void rotSimRight() {
-		// TODO Auto-generated method stub
-		
-	}
+            this.attFb(novoFilho, "INSERT");
 
-	@Override
-	public void displayAVL() {
-		// TODO Auto-generated method stub
-		
-	}
+            this.size++;
+            return true;
+        }
+
+        this.root = new NoAVL(elem, null, null, null, 0);
+        this.size++;
+        return true;
+    }
+
+    @Override
+    public void attFb(NoAVL no, String op) {
+
+        if (op.equals("INSERT")) {
+            if (this.isRoot(no) || ((NoAVL) no.getPai()).getFatorB() == 0) return;
+                 // CODNIÇÃO DE FINAL ENCONTROU O RAIZ OU UM PAI COM FATOR == 0
+        } else {
+            if (this.isRoot(no) || ((NoAVL) no.getPai()).getFatorB() != 0) return;
+        }
+        
+        int fatorPai = ((NoAVL) no.getPai()).getFatorB();
+        if ((int) no.getPai().getElemento() < (int) no.getElemento()) {
+            ((NoAVL) no.getPai()).setFatorB((op.equals("INSERT") ? --fatorPai : ++fatorPai));
+        } else {
+            ((NoAVL) no.getPai()).setFatorB((op.equals("INSERT") ? ++fatorPai : --fatorPai));
+        }
+
+        if (((NoAVL) no.getPai()).getFatorB() > 1 || ((NoAVL) no.getPai()).getFatorB() < - 1) // tem que balancear
+        
+        this.attFb((NoAVL) no.getPai(), op); // recusao
+    }
+
+    @Override
+    public void rotSimLeft() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void rotSimRight() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void displayAVL() {
+        // TODO Auto-generated method stub
+
+    }
 
 }
