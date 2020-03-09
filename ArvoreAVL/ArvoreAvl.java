0c0b66ca23f1b8c6411f2f13b6a9fe4dcbc76529
Igor Alisson <igoralissoncn@gmail.com>
2020-03-09 19:49:31 -0300

    Editando a interface da arvoreAVL

--- ArvoreAVL/ArvoreAvl.java
+++ ArvoreAVL/ArvoreAvl.java
@@ -3,8 +3,8 @@ package ArvoreAVL;
 public interface ArvoreAvl {
 
     public void attFb(NoAVL no, String op);
-    public void balancear(NoAVL pai);
-    public void rotSimLeft(NoAVL pai);
-    public void rotSimRight();
+    public void balancear(NoAVL noDesregulado);
+    public void rotSimLeft(NoAVL noDesregulado);
+    public void rotSimRight(NoAVL noDesregulado);
     public void displayAVL();
 }
