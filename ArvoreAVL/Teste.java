59fe087ec394719bc21dcb17fdcc1abcd57079ca
Igor Alisson <igoralissoncn@gmail.com>
2020-03-09 19:48:56 -0300

    Adicionando classe de teste

---
+++ ArvoreAVL/Teste.java
@@ -0,0 +1,18 @@
+
+package ArvoreAVL;
+
+
+public class Teste {
+
+    public static void main(String[] args) {
+        ArvoreAvllmpl nova = new ArvoreAvllmpl();
+        
+        nova.inserir(10);
+        nova.inserir(7);
+        nova.inserir(3);
+        
+        nova.displayAVL();
+        
+    }
+    
+}
