f633da662e7a853456a156a61f390d102b24b89a
Igor Alisson <igoralissoncn@gmail.com>
2020-03-11 16:22:13 -0300

    testando metodo remover da avl

--- ArvoreAVL/Teste.java
+++ ArvoreAVL/Teste.java
@@ -1,19 +1,29 @@
 
 package ArvoreAVL;
 
+import ArvoreBinaria.ElNaoEncontradoException;
 
 public class Teste {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ElNaoEncontradoException {
         ArvoreAvllmpl nova = new ArvoreAvllmpl();
         
         nova.inserir(50);
-        nova.inserir(70);
+        nova.inserir(90);
         nova.inserir(20);
-        nova.inserir(30);
+        nova.inserir(40);
         nova.inserir(10);
+        nova.inserir(30);
+        nova.inserir(62);
+        
         
         nova.displayAVL();
+        
+        nova.remover(62);
+        
+        System.out.println("----- POS EXCLUSAO -----");
+        
+       	nova.displayAVL();
         
     }
     
